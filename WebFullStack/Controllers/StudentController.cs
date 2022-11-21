37fd419e586d3a07b9c0ec97fa84edcdf8adb92f
quan91 <quan91.nt@gmail.com>
2022-11-21 22:48:23 +0700

    Buổi 4+5 : ActionResult and Views

--- WebFullStack/Controllers/StudentController.cs
+++ WebFullStack/Controllers/StudentController.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebFullStack.Models;
 
 namespace WebFullStack.Controllers
 {
+    [Log]
     public class StudentController : Controller
     {
         // GET: Student
         [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var model = new List<StudentInfor>();
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    model.Add(new StudentInfor { Id = i, Name = "Web ASPNET MVC thầy Quân bài " + i });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return View(model);
+        }
+
+        public ActionResult PartialViewDemo()
+        {
+            return PartialView();
         }
 
 
@@ -26,7 +46,7 @@ namespace WebFullStack.Controllers
         [NonAction]
         public ActionResult Index1(string id)
         {
-            return Json(null,JsonRequestBehavior.AllowGet);
+            return Json(null, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ WebFullStack/Models/StudentInfor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFullStack.Models
+{
+    public class StudentInfor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
